a91f3e339756e83fb2174b4d008c803ab4fd218b
Ola <148666180+nuraulaola@users.noreply.github.com>
2023-12-20 16:06:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.markdown(
     """
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://bootswatch.com/4/morph/bootstrap.min.css">
+    <link rel="stylesheet" href="https://bootswatch.com/morph/bootstrap.min.css">
     """
 )
 
