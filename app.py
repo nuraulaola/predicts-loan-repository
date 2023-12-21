563487ac11cdb5937a26c09adbb487bc9dcbcede
Ola <148666180+nuraulaola@users.noreply.github.com>
2023-12-21 13:14:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,21 +24,20 @@ st.set_page_config(
     page_icon="🗝️",
     layout="wide",
     initial_sidebar_state="expanded",
-    menu_items = {
-    'About': "This app provides predictions for loan eligibility based on user input."
-}
+    menu_items={'About': "This app provides predictions for loan eligibility based on user input."}
+)
 
 # Set up main page
 col1, col2 = st.columns((6, 1))
 col1.title("Loan Eligibility Prediction Assistant 🦉")
 col2.image("assets/Streamlit Loan Pred Logo (3)", width=120)
 st.sidebar.image("assets/Streamlit Loan Pred Logo (1).png")
-action = st.sidebar.radio("What would you like to do?", ("Run Loan Eligibility Prediction 📊")
+action = st.sidebar.radio("What would you like to do?", ("Run Loan Eligibility Prediction 📊",))
 
 st.markdown("Welcome to our Loan Prediction App!")
 st.markdown("This cutting-edge tool empowers our insurance team to accurately predict loan eligibility. Discover the future of finance at your fingertips!")
 st.markdown("Unleash the power of algorithms to make data-driven decisions. 🤖💡")
-                          
+
 html_temp = """
             <div style="background-color:white; padding:20px; border-radius:10px;">
                 <h1 style="color:black; text-align:left;">
@@ -58,11 +57,11 @@ def main():
 
     if choice == "Home":
         st.subheader("🏠 Home")
-        st.markdown(desc_temp, unsafe_allow_html=True)
+        # You need to define desc_temp somewhere before using it
+        # st.markdown(desc_temp, unsafe_allow_html=True)
     elif choice == "Machine Learning":
         st.subheader("🚀 Machine Learning")
         run_ml_app()
-    
 
 def run_ml_app():
     design = f"""<div style='padding:15px; background-color: white; border-radius:10px;'>
@@ -72,10 +71,10 @@ def run_ml_app():
 
     # Use st.columns to create two side-by-side columns
     left, right = st.columns(2)
-    
+
     # User inputs with improved styling
     gender = left.selectbox('Gender', ('Male', 'Female'))
-    married = right.selectbox('Married', ('Yes','No'))
+    married = right.selectbox('Married', ('Yes', 'No'))
     dependent = left.selectbox('Dependents', ('None', 'One', 'Two', 'Three'))
     education = right.selectbox('Education', ('Graduate', 'Non-Graduate'))
     self_employed = left.selectbox('Self-Employed', ('Yes', 'No'))
@@ -84,8 +83,8 @@ def run_ml_app():
     loan_amount = right.number_input('Loan Amount', value=0, step=100, format='%d')
     loan_amount_term = left.number_input('Loan Tenor (In Months)', value=0, step=12, format='%d')
     credit_history = right.number_input('Credit History', 0.0, 1.0, value=0.0, step=0.1)
-    property_area = st.selectbox('Property Area', ('Semiurban','Urban', 'Rural'))
-    
+    property_area = st.selectbox('Property Area', ('Semiurban', 'Urban', 'Rural'))
+
     # Predict button with enhanced styling
     button = st.button('Predict', key='predict_button', help='Click to predict loan eligibility')
 
@@ -101,22 +100,21 @@ def run_ml_app():
         else:
             st.warning(f'⚠️ Sorry, you are {result} for the loan. Please review your inputs.')
 
-
-def predict(gender,married,dependent,education, self_employed, applicant_income,coApplicant_income,
-                         loan_amount, loan_amount_term, credit_history,property_area):
-    #processing user input
+def predict(gender, married, dependent, education, self_employed, applicant_income, coApplicant_income,
+            loan_amount, loan_amount_term, credit_history, property_area):
+    # processing user input
     gen = 0 if gender == 'Male' else 1
     mar = 0 if married == 'Yes' else 1
     dep = float(0 if dependent == 'None' else 1 if dependent == 'One' else 2 if dependent == 'Two' else 3)
     edu = 0 if education == 'Graduate' else 1
     sem = 0 if self_employed == 'Yes' else 1
     pro = 0 if property_area == 'Semiurban' else 1 if property_area == 'Urban' else 2
-    lam = loan_amount/1000
+    lam = loan_amount / 1000
     cap = coApplicant_income / 1000
 
-    #Making prediction
-    prediction = Random_Forest_Model.predict([[gen, mar, dep, edu, sem, applicant_income, cap, lam, loan_amount_term
-                                               , credit_history, pro]])
+    # Making prediction
+    prediction = Random_Forest_Model.predict(
+        [[gen, mar, dep, edu, sem, applicant_income, cap, lam, loan_amount_term, credit_history, pro]])
     result = 'Not Eligible' if prediction == 0 else 'Eligible'
 
     return result
