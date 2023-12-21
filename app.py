5faceb11d9532f44d84f988d25fb503890385248
Ola <148666180+nuraulaola@users.noreply.github.com>
2023-12-21 14:20:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -51,6 +51,57 @@ st.markdown("Welcome to our Loan Prediction App!")
 st.markdown("This cutting-edge tool empowers our insurance team to accurately predict loan eligibility. Discover the future of finance at your fingertips!")
 st.markdown("Unleash the power of algorithms to make data-driven decisions. 🤖💡")
 
+def run_ml_app():
+    # Use st.columns to create two side-by-side columns
+    left, right = st.columns(2)
+
+    # User inputs with improved styling
+    gender = left.selectbox('Gender', ('Male', 'Female'))
+    married = right.selectbox('Married', ('Yes', 'No'))
+    dependent = left.selectbox('Dependents', ('None', 'One', 'Two', 'Three'))
+    education = right.selectbox('Education', ('Graduate', 'Non-Graduate'))
+    self_employed = left.selectbox('Self-Employed', ('Yes', 'No'))
+    applicant_income = right.number_input('Applicant Income', value=0, step=100, format='%d')
+    coApplicant_income = left.number_input('Co-Applicant Income', value=0, step=100, format='%d')
+    loan_amount = right.number_input('Loan Amount', value=0, step=100, format='%d')
+    loan_amount_term = left.number_input('Loan Tenor (In Months)', value=0, step=12, format='%d')
+    credit_history = right.number_input('Credit History', 0.0, 1.0, value=0.0, step=0.1)
+    property_area = st.selectbox('Property Area', ('Semiurban', 'Urban', 'Rural'))
+
+    # Predict button with enhanced styling
+    button = st.button('Predict', key='predict_button', help='Click to predict loan eligibility')
+
+    # If the Predict button is clicked
+    if button:
+        # Make prediction
+        result = predict(gender, married, dependent, education, self_employed, applicant_income, coApplicant_income,
+                         loan_amount, loan_amount_term, credit_history, property_area)
+
+        # Display result with improved styling
+        if result == 'Eligible':
+            st.success(f'🎉 Congratulations! You are {result} for the loan.')
+        else:
+            st.warning(f'⚠️ Sorry, you are {result} for the loan. Please review your inputs.')
+
+def predict(gender, married, dependent, education, self_employed, applicant_income, coApplicant_income,
+            loan_amount, loan_amount_term, credit_history, property_area):
+    # processing user input
+    gen = 0 if gender == 'Male' else 1
+    mar = 0 if married == 'Yes' else 1
+    dep = float(0 if dependent == 'None' else 1 if dependent == 'One' else 2 if dependent == 'Two' else 3)
+    edu = 0 if education == 'Graduate' else 1
+    sem = 0 if self_employed == 'Yes' else 1
+    pro = 0 if property_area == 'Semiurban' else 1 if property_area == 'Urban' else 2
+    lam = loan_amount / 1000
+    cap = coApplicant_income / 1000
+
+    # Making prediction
+    prediction = Random_Forest_Model.predict(
+        [[gen, mar, dep, edu, sem, applicant_income, cap, lam, loan_amount_term, credit_history, pro]])
+    result = 'Not Eligible' if prediction == 0 else 'Eligible'
+
+    return result
+                
 def main():
     run_ml_app()
     
@@ -124,57 +175,6 @@ elif action == "Feedback 🌟":
     if st.button("Submit Feedback"):
         # Process and store the feedback (you can customize this part based on your needs)
         st.success("Thank you for your feedback!")
-
-def run_ml_app():
-    # Use st.columns to create two side-by-side columns
-    left, right = st.columns(2)
-
-    # User inputs with improved styling
-    gender = left.selectbox('Gender', ('Male', 'Female'))
-    married = right.selectbox('Married', ('Yes', 'No'))
-    dependent = left.selectbox('Dependents', ('None', 'One', 'Two', 'Three'))
-    education = right.selectbox('Education', ('Graduate', 'Non-Graduate'))
-    self_employed = left.selectbox('Self-Employed', ('Yes', 'No'))
-    applicant_income = right.number_input('Applicant Income', value=0, step=100, format='%d')
-    coApplicant_income = left.number_input('Co-Applicant Income', value=0, step=100, format='%d')
-    loan_amount = right.number_input('Loan Amount', value=0, step=100, format='%d')
-    loan_amount_term = left.number_input('Loan Tenor (In Months)', value=0, step=12, format='%d')
-    credit_history = right.number_input('Credit History', 0.0, 1.0, value=0.0, step=0.1)
-    property_area = st.selectbox('Property Area', ('Semiurban', 'Urban', 'Rural'))
-
-    # Predict button with enhanced styling
-    button = st.button('Predict', key='predict_button', help='Click to predict loan eligibility')
-
-    # If the Predict button is clicked
-    if button:
-        # Make prediction
-        result = predict(gender, married, dependent, education, self_employed, applicant_income, coApplicant_income,
-                         loan_amount, loan_amount_term, credit_history, property_area)
-
-        # Display result with improved styling
-        if result == 'Eligible':
-            st.success(f'🎉 Congratulations! You are {result} for the loan.')
-        else:
-            st.warning(f'⚠️ Sorry, you are {result} for the loan. Please review your inputs.')
-
-def predict(gender, married, dependent, education, self_employed, applicant_income, coApplicant_income,
-            loan_amount, loan_amount_term, credit_history, property_area):
-    # processing user input
-    gen = 0 if gender == 'Male' else 1
-    mar = 0 if married == 'Yes' else 1
-    dep = float(0 if dependent == 'None' else 1 if dependent == 'One' else 2 if dependent == 'Two' else 3)
-    edu = 0 if education == 'Graduate' else 1
-    sem = 0 if self_employed == 'Yes' else 1
-    pro = 0 if property_area == 'Semiurban' else 1 if property_area == 'Urban' else 2
-    lam = loan_amount / 1000
-    cap = coApplicant_income / 1000
-
-    # Making prediction
-    prediction = Random_Forest_Model.predict(
-        [[gen, mar, dep, edu, sem, applicant_income, cap, lam, loan_amount_term, credit_history, pro]])
-    result = 'Not Eligible' if prediction == 0 else 'Eligible'
-
-    return result
 
 if __name__ == "__main__":
     main()
